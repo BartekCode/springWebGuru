256c3389d16f5beaa2332a94441a8b4af0037aca
Bartek <szkudlarz@onet.pl>
2022-01-09 21:26:48 +0100

    initial commit

---
+++ src/main/java/com/example/springwebguru/SpringWebGuruApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebGuruApplication.class, args);
+    }
+
+}
